7a04d25cf5d49a9f0a6bb0629dd24ff921d18c94
Otis <ledat02072005@gmail.com>
2025-11-11 21:45:06 +0700

    pingpong and tracing

---
+++ user/trace.c
@@ -0,0 +1,19 @@
+#include "kernel/param.h"
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  if(argc < 3){
+    fprintf(2, "Usage: trace mask command [args]\n");
+    exit(1);
+  }
+
+  int mask = atoi(argv[1]);
+  trace(mask);
+
+  exec(argv[2], argv+2);
+  exit(0);
+}
